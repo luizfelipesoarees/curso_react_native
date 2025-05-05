75004841705305b21f895ddc9a6dfcf067355098
luizfelipesoarees <luizfelipesoarees@gmail.com>
2025-05-05 17:49:25 -0300

    formulário de usuário

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,53 @@
-import React from 'react'
-import { Text } from "react-native"
+import React, { useState } from 'react'
+import { Text, View, TextInput, StyleSheet, Button } from "react-native"
 
-export default props => {
+export default ({route, navigation}) => {
+     const [user, setUser] = useState(route.params ? route.params : {})
      return (
-        <Text>UserForm</Text>
+        <View style={style.form}>
+            <Text>Name</Text>
+            <TextInput
+            style={style.input}
+               onChangeText={name => (setUser({...user, name}))}
+               placeholder='Informe o Nome'
+               value={user.name}
+            />
+
+            <Text>Emaile</Text>
+            <TextInput
+            style={style.input}
+               onChangeText={email => (setUser({...user, email}))}
+               placeholder='Informe o E-mail'
+               value={user.email}
+            />
+
+            <Text>URL do Avatar</Text>
+            <TextInput
+            style={style.input}
+               onChangeText={avatarUrl => (setUser({...user, avatarUrl}))}
+               placeholder='Informe a URL do Avatar'
+               value={user.avatarUrl}
+            />
+
+            <Button 
+               title='Salvar'
+               onPress={() => {
+                  navigation.goBack()
+               }}
+            />
+        </View>
+        
      )
 }
+
+const style = StyleSheet.create({
+   form: {
+      padding: 12
+   },
+   input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 15,
+   }
+})
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,33 +1,50 @@
 import React from 'react'
-import { View, FlatList } from "react-native"
+import { View, FlatList, Alert } from 'react-native'
 import users from '../data/users'
-import { ListItem, Avatar, Button } from 'react-native-elements'
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements'
 
 export default props => {
 
-function getActions(user) {
-   return (
-      <>
-         <Button
-            onPress={() => props.navigation.navigate('UserForm', user)}
-         />
-      </>
-   )
-}
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn('delete ' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </>
+        )
+    }
 
     function getUserItem({ item: user }) {
         return (
-            <ListItem
-                key={user.id}
-                bottomDivider
-                onPress={() => props.navigation.navigate('UserForm')}
-            >
+            <ListItem bottomDivider onPress={() => props.navigation.navigate('UserForm', user)}>
                 <Avatar source={{ uri: user.avatarUrl }} rounded />
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
-                    rightElement={getActions(user)}
                 </ListItem.Content>
+                {getActions(user)}
             </ListItem>
         )
     }
